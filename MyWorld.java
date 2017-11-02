b340754146c55e306c48676d52650a136b54d0b3
Polomij <31891495+Polomij@users.noreply.github.com>
2017-11-02 15:08:28 +0100

    02.11.2017

--- MyWorld.java
+++ MyWorld.java
@@ -1,31 +1,16 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public void showText(java.lang.String text,int x,int y){}
+
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1500, 800, 1); 
         prepare();
-        
+
     }
 
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
     private void prepare()
     {
         Adam adam = new Adam();
@@ -34,8 +19,38 @@ public class MyWorld extends World
         addObject(eva,1306,700);
         Drzewo drzewo = new Drzewo();
         addObject(drzewo,1300,165);
-
         Toaleta toaleta = new Toaleta();
         addObject(toaleta,128,695);
+    }
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("k"))
+        {
+            int szerokosc = getWidth();
+            int wysokosc = getHeight();
+            String odp = Greenfoot.ask("Ile wonszy mirordzie?");
+            int ile=Integer.parseInt(odp);
+            if(ile>5)ile=5;
+            for(int i = 0; i< ile; i++)
+            {
+                int x = Greenfoot.getRandomNumber(szerokosc);
+                int y = Greenfoot.getRandomNumber(wysokosc);
+                Snake snake = new Snake ();
+                addObject(snake, x, y);
+                while(snake.dotykaAdama())
+                {
+                    x = Greenfoot.getRandomNumber(szerokosc);
+                    y = Greenfoot.getRandomNumber(wysokosc);
+                    snake.setLocation(x,y);
+                }
+                while(snake.dotykaEwe())
+                {
+                    x = Greenfoot.getRandomNumber(szerokosc);
+                    y = Greenfoot.getRandomNumber(wysokosc);
+                    snake.setLocation(x,y);
+                }
+            }
+        }
     }
 }
